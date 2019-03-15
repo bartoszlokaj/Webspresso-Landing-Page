139f2e6691502de530736d9b99edeb4f8451a4dc
web <lysy2017!@>
2019-03-15 15:46:34 +0100

    positioning

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 let coll = document.querySelectorAll(".collapsible__btn");
-let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function(){
     this.classList.toggle("active");
     let content = this.nextElementSibling;
-    if (content.style.display === "block") {
+    if (content.style.display === "block" && content.style.maxHeight) {
       content.style.display = "none";
+      content.style.maxHeight = null;
     } else {
       content.style.display = "block";
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
 }
