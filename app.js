f7ae4a112d17df65f95ffd8e42a87ade17d51acb
Bartosz <bartek@gmail.com>
2019-03-15 21:47:48 +0100

    arrow animation

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let colls = document.querySelectorAll(".collapsible__btn");
+  let colls = document.querySelectorAll(".collapsible__btn"); 
 
   colls.forEach((coll) => {
     let content = coll.nextElementSibling; // content = collapsible__wrapper
+    let arrow = coll.querySelector('.collapsible__arrow');
     const elementHeight = content.clientHeight; // collapsible__wrapper height
     content.style.maxHeight = '0';
     coll.addEventListener('click', () => {
       content.classList.add('collapsible__wrapper--animated');
       coll.classList.toggle('active');
+      arrow.classList.toggle('collapsible__arrow--animated');
       if (content.style.maxHeight === elementHeight + 'px') {
         content.style.maxHeight = '0px';
       } else {
