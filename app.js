51a62c0484bc23e544b4e802d904f1c35b2786cc
Bartosz <bartek@gmail.com>
2019-03-15 21:41:24 +0100

    Ariel animation fix

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-let coll = document.querySelectorAll(".collapsible__btn");
+document.addEventListener('DOMContentLoaded', () => {
+  let colls = document.querySelectorAll(".collapsible__btn");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function(){
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.display === "block" && content.style.maxHeight) {
-      content.style.display = "none";
-      content.style.maxHeight = null;
-    } else {
-      content.style.display = "block";
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+  colls.forEach((coll) => {
+    let content = coll.nextElementSibling; // content = collapsible__wrapper
+    const elementHeight = content.clientHeight; // collapsible__wrapper height
+    content.style.maxHeight = '0';
+    coll.addEventListener('click', () => {
+      content.classList.add('collapsible__wrapper--animated');
+      coll.classList.toggle('active');
+      if (content.style.maxHeight === elementHeight + 'px') {
+        content.style.maxHeight = '0px';
+      } else {
+        content.style.maxHeight = elementHeight + 'px';
+      }
+    });
   });
-}
-
+});
