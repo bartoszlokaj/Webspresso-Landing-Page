d62de0a1658fa80014c10c3f0fb3ea1a659b182e
Bartosz <bartoszlokaj@gmail.com>
2019-03-21 14:53:22 +0100

    animation top bar

--- js/topNav.js
+++ js/topNav.js
@@ -1,10 +1,9 @@
-
-
 function myFunction() {
     let x = document.querySelector(".header__list");
     if (x.className === "header__list") {
-      x.className += " responsive";
+      x.classList.toggle('responsive');
+    //   x.classList.toggle('responsive--animated');
     } else {
-      x.className = "header__list";
+        x.className = "header__list";
     }
   }
