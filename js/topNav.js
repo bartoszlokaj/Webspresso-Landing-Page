9f74f90b258fabccaf3e21ac5014c2d9672bfe7c
Bartosz <bartoszlokaj@gmail.com>
2019-03-21 16:19:50 +0100

    animation fix topbar

--- js/topNav.js
+++ js/topNav.js
@@ -3,7 +3,10 @@ function myFunction() {
     if (x.className === "header__list") {
       x.classList.toggle('responsive');
     //   x.classList.toggle('responsive--animated');
+    } else if (x.className === "header__list responsive") {
+        x.classList.remove('responsive');
+        x.classList.toggle('responsive--animated');
     } else {
-        x.className = "header__list";
+        x.className = "header__list responsive";
     }
   }
